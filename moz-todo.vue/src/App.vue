4d58d9bf984f9df34a6862594f9f0f7039ff4809
mcelam00 <mcelam00@estudiantes.unileon.es>
2021-06-04 18:23:57 +0200

    Primera página del tutorial hecha

--- moz-todo.vue/src/App.vue
+++ moz-todo.vue/src/App.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To-Do list</h1>
+   <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> --><!-- esto es una componente que importamos en la segunda parte-->
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+
+
+
+
+<script>
+/*import HelloWorld from './components/HelloWorld.vue' //importamos el  helloworld que es una componente que hemos usado en el template
+
+export default { //exportamos la componente. Esto setea el nombre de la componente a App
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld //y registramos la componente HelloWorld añadiendola a la propiedad de las componentes
+    //esto significa que se registra localmente y solo se puede usar dentro de la componente en la que se registra
+    //habria que importar y registrar la componente Helloworld en este caso en cada otro archivo componente que la use. 
   }
-}
+}*/
 </script>
+
+<!-- Todo el código que no vemos en ésta componente está en la Helloworld es como si la incluyesemos aqui-->
+
+<!-- Nuestra página va tener que contar con distintas componentes, hacer un diseño de componentes y de que se encarga cada una-->
+
+<!-- Las componentes pueden tener propiedades y se pueden inicializar. Podemos tener una componente para cada tarea siendo las tareas distintas y tendriamos una instancia de la componente
+Las componentes se renderizan, se mandan a la pagina, se visualizan, pueden o no estar visualizandose (renderizaar)-->
+
 
 <style>
 #app {
@@ -26,3 +42,11 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+<!--esta es la componente principal, como si fuera la clase principal por donde comienza la entrada a la
+aplicacion.
+Estructura compartida de 3 partes por todas las componentes
+Hay tres partes, la parte de HTML que es el template, iría el codigo html ampliado con las cosas de vue
+En la segunda es el codigo javascript que se usa en esta componente
+Por último la parte de estilo que es CSS, en VUE se mantienen separados los modulos que hacen la app y son las componentes y se haran en ellas las tareas-->
--- moz-todo.vue/src/main.js
+++ moz-todo.vue/src/main.js
@@ -5,4 +5,4 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')//esto es el App.vue que es la componente de mas alto nivel de donde cuelga todo (es un diseño basado en componentes)
